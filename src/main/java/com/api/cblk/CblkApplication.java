ee212aaf9976145d6aca0b4ceda7da8e24360418
Cleber Pereira <cleberpereiraeng@gmail.com>
2023-08-23 18:28:06 -0300

    <build> First commit, build structure of the project

---
+++ src/main/java/com/api/cblk/CblkApplication.java
@@ -0,0 +1,13 @@
+package com.api.cblk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CblkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CblkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/cblk/CblkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cblk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CblkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
